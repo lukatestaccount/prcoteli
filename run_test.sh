a9974f49f3408f2bce6cc7b83031f5c9bb04cd32
Luka Kalinovcic <kalinov@google.com>
2015-12-11 15:26:15 +0100

    a

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-g++ impl/concrete/$1_test.cpp -I. -o bin/concrete/$1_test -O2 -std=c++11 -lm && bin/concrete/$1_test --test_only "${@:2}"
+g++ impl/concrete/$1_test.cpp -I. -o test -O2 -std=c++11 -lm && ./test --test_only "${@:2}" && rm test
